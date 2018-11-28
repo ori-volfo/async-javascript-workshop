3fd4d900e13b16bfb2357046e6cff404db9831d1
Ori <ori.volfo@gmail.com>
2018-11-28 12:26:19 +0200

    promise quiz #2

--- node/testing.js
+++ node/testing.js
@@ -1,13 +1,24 @@
 const fs = require("fs");
+const zlib = require("zlib");
 
-function readFile(filename, encoding) {
+function zlibPromise(data) {
     return new Promise((resolve, reject) => {
-        fs.readFile(filename, encoding, (err, data) => {
-            if (err) reject(err); // pass values
-            resolve(data); // pass values
+        zlib.gzip(data, (error, result) => {
+            if (error) reject(error);
+            resolve(result);
         });
     });
 
 }
+
+function readFile(filename, encoding) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filename, encoding, (err, data) => {
+            if (err) reject(err);
+            resolve(data);
+        });
+    });
+}
+
 readFile(__dirname+"/files/demofile.txt", "utf-8")
-    .then(val => console.log(val), err => console.error(err));
+    .then(data => {console.log(data);return(zlibPromise(data));}).then(zipped => console.log(zipped)); // --> Load it then zip it and then print it to screen
