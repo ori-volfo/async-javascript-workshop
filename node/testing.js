4c80c92682920167f7d80de4b7a06302cb162370
Ori <ori.volfo@gmail.com>
2018-11-28 17:13:52 +0200

    promise quiz #4

--- node/testing.js
+++ node/testing.js
@@ -4,7 +4,7 @@ const zlib = require("zlib");
 function zlibPromise(data) {
     return new Promise((resolve, reject) => {
         zlib.gzip(data, (error, result) => {
-            if (error) reject(error);
+            if (error) throw error;
             resolve(result);
         });
     });
@@ -14,11 +14,13 @@ function zlibPromise(data) {
 function readFile(filename, encoding) {
     return new Promise((resolve, reject) => {
         fs.readFile(filename, encoding, (err, data) => {
-            if (err) reject(err);
+            if (err) throw err;
             resolve(data);
         });
     });
 }
 
 readFile(__dirname+"/files/demofile.txt", "utf-8")
-    .then(data => {console.log(data);return(zlibPromise(data));}).then(zipped => console.log(zipped)); // --> Load it then zip it and then print it to screen
+    .then(data => {console.log(data);return(zlibPromise(data));})
+    .then(zipped => console.log(zipped))
+    .catch(); // --> Load it then zip it and then print it to screen
