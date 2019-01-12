5c417fe5203571abcb3c15f73b96e899e17c958b
Ori <ori.volfo@gmail.com>
2019-01-12 17:49:55 +0200

    node loop quiz #2

--- node/node-loop.js
+++ node/node-loop.js
@@ -7,7 +7,21 @@ const interval = setInterval(() => {
         })
         .then(()=>{
             console.log('promise 2');
-            clearInterval(interval);
+            setImmediate(()=>{
+                console.log('setImmediate 1');
+                Promise.resolve()
+                    .then(()=>{
+                        console.log('promise 3');
+                    })
+                    .then(()=>{
+                        console.log('promise 4');
+                        clearInterval(interval);
+                    });
+            });
+            process.nextTick(()=>{
+                console.log('process nextTick');
+            });
+
         });
 }, 0);
 console.log("end");
